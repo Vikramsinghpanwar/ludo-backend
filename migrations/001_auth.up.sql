33de36bb36b6fef6e7be7aeed99b27594e695b3a
vikramsinghpanwar <vikramsinghpanwar000@gmail.com>
2025-12-31 21:29:00 +0530

    Implemented OTP Authentication with Security enhancement

--- migrations/001_auth.up.sql
+++ migrations/001_auth.up.sql
@@ -15,3 +15,18 @@ CREATE TABLE refresh_tokens (
     expires_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
+
+CREATE TABLE auth_otp_requests (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    phone VARCHAR(15) NOT NULL UNIQUE,
+    purpose VARCHAR(20) NOT NULL,
+
+    otp_hash TEXT NOT NULL,
+    attempts INT NOT NULL DEFAULT 0,
+    verify_attempts INT NOT NULL DEFAULT 0,
+    otp_verified BOOLEAN NOT NULL DEFAULT FALSE,
+    last_sent_at TIMESTAMP NOT NULL,
+
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
